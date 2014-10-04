10985986301d99af9a49a75e3c0fceaa8a478660
wdarvin <wesdarvin@gmail.com>
2014-10-04 10:15:11 -0400

    actual working script for final table format

--- csvImportScript.py
+++ csvImportScript.py
@@ -2,52 +2,61 @@ import numpy as np
 import MySQLdb as mdb
 import sys
 import time
-
-#Define tic-tok function
-def tic():
-    #Homemade version of matlab tic and toc functions
-    global startTime_for_tictoc
-    startTime_for_tictoc = time.time()
-
-def toc():
-    if 'startTime_for_tictoc' in globals():
-        print "Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds."
-    else:
-        print "Toc: start time not set"
-
+import datetime
 
 
 #load data file, change path for beaglebone
-file = "C:\Users\wes\Documents\BuckeyeCurrent\HackAThon\data.csv"
+file = "C:\Users\wes\Documents\BuckeyeCurrent\CANCorder-UI\NormalData.csv"
 
 #create matrix containing data from file
-data = np.loadtxt(file, dtype=float, delimiter=',')
+data = np.loadtxt(file, dtype=float, delimiter=',', skiprows = 1)
 
 #load matrix into variable arrays
 #+- statements for more/different data
-voltage = data
+PhaseAtemp = data[:,2]
+BusVoltage = data[:,3]
+MotorId = data[:,4]
+MotorTemp = data[:,5]
+MotorVelocity = data[:,6]
+PackTemp = data[:,6]
+PackSOC = data[:,7]
+PackBalance = data[:,8]
+PrechargeCont = data[:,9]
+MainCont = data[:,10]
+EStop = data[:,11]
 
 
 #establish connection with database
 
-con = mdb.connect('localhost','root','buckeyes','test_tutorial')
+con = mdb.connect('localhost','root','buckeyes','test')
 
-tic()
+
 with con:
     #establish pointer? I think that's what this does
     cur = con.cursor()
 
     #Drop table Example if it exists
-    cur.execute("DROP TABLE IF EXISTS Example")
+    cur.execute("DROP TABLE IF EXISTS Messages")
     
     #creates table, will not need for real script
-    cur.execute("CREATE TABLE Example(time datetime(6), voltage float, three float)")
-    
-    
-    for i in range(0,100):
-        #insert time variable into table
-        cur.execute('INSERT INTO Example(time, voltage, three) VALUES (now(6), 1, 1)')
-        #time.sleep(.5)
+    cur.execute("CREATE TABLE Messages(time datetime(6), MsgName TEXT, Value float)")
 
 
-toc()
+    for i in range(0,500):
+        cur.executemany('''INSERT INTO Messages(time, MsgName, Value)
+                        VALUES(%s,%s,%s)''',
+        [
+        (str(datetime.datetime.now()),'PhaseAtemp',str(PhaseAtemp[i])),
+        (str(datetime.datetime.now()),'BusVoltage',str(BusVoltage[i])),
+        (str(datetime.datetime.now()),'MotorId',str(MotorId[i])),
+        (str(datetime.datetime.now()),'MotorTemp',str(MotorTemp[i])),
+        (str(datetime.datetime.now()),'MotorVelocity',str(MotorVelocity[i])),
+        (str(datetime.datetime.now()),'PackTemp',str(PackTemp[i])),
+        (str(datetime.datetime.now()),'PackSOC',str(PackSOC[i])),
+        (str(datetime.datetime.now()),'PackBalance',str(PackBalance[i])),
+        (str(datetime.datetime.now()),'PrechargeCont',str(PrechargeCont[i])),
+        (str(datetime.datetime.now()),'MainCont',str(MainCont[i])),
+        (str(datetime.datetime.now()),'EStop',str(EStop[i]))
+        ])
+        time.sleep(.5)
+
