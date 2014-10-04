2932ff465849dfbdc18211fdef750198860b6674
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2014-10-04 05:09:30 -0400

    disable beagle bone services

--- home/cancorder/setup.sh
+++ home/cancorder/setup.sh
@@ -5,3 +5,8 @@
 sudo a2enmod rewrite
 sudo apt-get install mysql-server-5.6 phpmyadmin
 # use password “buckeyes”
+
+# beaglebone-specific config
+sudo systemctl disable cloud9.service
+sudo systemctl disable bone101.service
+sudo systemctl disable gateone.service
