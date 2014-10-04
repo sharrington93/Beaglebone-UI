2734c31eb05439c43751fa2dce1df2e7e03a78b5
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2014-10-04 08:57:37 -0400

    add script to automatically update BeagleBone from github

---
+++ home/cancorder/update_from_github.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# Download the latest master from the GitHub repo, then unpack it to the root filesystem.
+# WARNING: this will happily overwrite anything in its path.
+
+mkdir -p ~/tmp
+cd ~/tmp
+wget https://github.com/BuckeyeCurrent/CANCorder-UI/archive/master.zip
+unzip CANCorder-UI-master.zip
+sudo rsync -a CANCorder-UI-master/ /
+
+sudo /etc/init.d/apache2 restart
+sudo /etc/init.d/mysql restart
